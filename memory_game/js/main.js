6fc2d1322998373c10947cfebbcc6b3447bda5b6
Solito <solito-air@RG-MbAir.local>
2019-02-17 19:26:23 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,30 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+console.log("User flipped " + 'cardOne');
+console.log("User flipped " + 'cardThree');
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+
+var cardTwo = cards[3];
+cardsInPlay.push('cardTwo');
+
+if (cardsInPlay.length === 2) {
+	alert('Hello, friends.');
+} else if 
+	(cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!")
+} else {
+	alert("Sorry, try again.")
+}
+
+
+
+
+
+
+
+
+
